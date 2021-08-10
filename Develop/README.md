413bd9431ca213bea00e033bc05e9e69074e7eff
micahwaweru <micah.waweru@gmail.com>
2021-08-09 21:59:53 -0500

    progress

--- Develop/README.md
+++ Develop/README.md
@@ -1,3 +1,15 @@
-# Micah Waweru
- ## GitHub: [https://github.com/micahwaweru]
-]
+# Micah's Project
+## Description: 
+ This is my description
+ ## Installation: 
+ Please install with care
+ ## Usage Information: 
+ Use as such
+ ## Contribution Guidlines 
+ Contribute wisely
+ ## Test Instructions 
+ test thoroughly
+## Questions: 
+Github: [https://github.com/micahwaweru]
+ ## Email:  
+ micah.waweru@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -13,20 +13,66 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        message: 'What is your GitHub Username?',
-        name: 'gitHubUN'
-    }
+        message: 'Give a short description',
+        name: 'desc'
+    },
+    {
+        type: 'input',
+        message: 'Please proved installation instructions',
+        name: 'instlIns'
+    },
+    {
+        type: 'input',
+        message: 'Please proved usage information',
+        name: 'usageInf'
+    },
+    {
+        type: 'input',
+        message: 'Please proved contribution guidlines',
+        name: 'contrGuide'
+    },
+    {
+        type: 'input',
+        message: 'Please proved test instructions',
+        name: 'testIns'
+    },
+    {
+        type: 'rawlist',
+        message: 'Please select a license.',
+        name: 'license',
+        choices: ['GNU', 'MIT']
+        
+    },
+    {
+        type: 'input',
+        message: 'Please provide your GitHub Username',
+        name: 'gitHubUN',
+    },
+    {
+        type: 'input',
+        message: 'Please provide your email address.',
+        name: 'email',
+    },
 ])
 .then((response)=>{
     console.log(response);
     var titleName = response.titleName;
     console.log(titleName);  
-    var gitHubUN = response.gitHubUN;
     
     
-    var titleString = `# ${titleName}`;
     
-    var readMetext;
+    var titleString = `# ${titleName}\n`;
+    var desc = `## Description: \n ${response.desc}\n`;
+    var instlIns = ` ## Installation: \n ${response.instlIns}\n`;
+    var usageInf = ` ## Usage Information: \n ${response.usageInf}\n`;
+    var contrGuide = ` ## Contribution Guidlines \n ${response.contrGuide}\n`;
+    var testIns = ` ## Test Instructions \n ${response.testIns}\n`;
+    var questions = `## Questions: \n`
+    var gitHubUN = `Github: [https://github.com/${response.gitHubUN}]\n`
+    var email = `Email:  \n ${response.email}\n`;
+    
+    
+    var readMetext = titleString + desc + instlIns + usageInf + contrGuide + testIns + + response.license +  questions + gitHubUN + email;
     
     
     
